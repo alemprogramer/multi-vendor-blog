3791cd1745004ed7e6171a5cae39fc6d68646cca
lm-hossain <aleam.hossain@mymdoc.com>
2024-04-08 13:25:38 +0600

    env setup

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
     router(req,res);
 })
 
-
-server.listen(3000,() => {
-    console.log(`server is running on port ${3000}`);
+const PORT =process.env.PORT || 3001
+server.listen(PORT,() => {
+    console.log(`server is running on port ${PORT}`);
 });
