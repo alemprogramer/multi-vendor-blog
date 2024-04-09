a31bdc4a9df92b97dd943ee82d2eabdc5eacc566
lm-hossain <aleam.hossain@mymdoc.com>
2024-04-09 15:41:57 +0600

    database connected

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const router = require('./routers/router');
-const parseData = require('./utils/dataParser')
+const parseData = require('./utils/dataParser');
+const mongoose = require('mongoose');
 
 const server = http.createServer((req, res) => {
     console.log(`${req.method} :${req.url}`);
@@ -8,6 +9,11 @@ const server = http.createServer((req, res) => {
 })
 
 const PORT =process.env.PORT || 3001
-server.listen(PORT,() => {
-    console.log(`server is running on port ${PORT}`);
-});
+mongoose.connect(process.env.database_url)
+.then(() =>{
+    console.log(`server is connected`);
+    server.listen(PORT,() => {
+        console.log(`server is running on port ${PORT}`);
+    });
+})
+
