94a4579de0910108d573a2869d702a330723a012
lm-hossain <aleam.hossain@mymdoc.com>
2024-04-09 16:41:32 +0600

    request color add

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const http = require('http');
 const router = require('./routers/router');
-const parseData = require('./utils/dataParser');
+const {parseData,color} = require('./utils/dataParser');
 const mongoose = require('mongoose');
 
 const server = http.createServer((req, res) => {
-    console.log(`${req.method} :${req.url}`);
+    const startTime = Date.now();
+    
     parseData(req,res,router);
+
+    res.on('finish', () => {
+        const endTime = Date.now(); 
+        const elapsedTime = endTime - startTime; 
+        console.log(`${req.method} ${req.url} ${color(res.statusCode)} \x1b[0m - ${elapsedTime}ms`);
+    })
 })
 
 const PORT =process.env.PORT || 3001
--- utils/dataParser.js
+++ utils/dataParser.js
@@ -1,4 +1,4 @@
-const parseData = (req,res,router)=>{
+exports.parseData = (req,res,router)=>{
     let data = '';
     
     req.on('data', (chunk) => {
@@ -11,8 +11,18 @@ const parseData = (req,res,router)=>{
         }
         router(req,res)
     });
-    console.log('here');
-    req.name="lm"
 }
-
-module.exports = parseData;
+exports.color = (code)=>{
+    if(code >= 200 && code <= 299){
+        return `\x1b[33m ${code}` //yellow
+    }else if(code >= 300 && code <= 399){
+        return `\x1b[32m ${code}` //green
+    }else if(code >= 400 && code <= 499){
+        return `\x1b[34m ${code}` //blue
+    }else if(code >= 500 && code <=599){
+        return `\x1b[31m ${code}` //red
+    }else{
+        return `\x1b[0m ${code}` //default
+    }
+}
+// module.exports = parseData;
