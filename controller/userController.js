aa00221a076ae7808460d5238fa691f105eb17f6
lm-hossain <aleam.hossain@mymdoc.com>
2024-04-10 16:41:37 +0600

    all get api done

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,17 @@
+const User = require("../models/User")
+
 exports.getAllUserController = async (req,res,next)=>{
     try {
-        console.log(req.user);
-        res.status(200).json({msg:"test message"})
+        const page = parseInt(req.query.page) || 1;
+        const pageSize = parseInt(req.query.pageSize) || 10;
+
+        const skip = (page - 1) * pageSize;
+        const user = await User.find().limit(pageSize).skip(skip).select('-password');
+        res.status(200).json({
+            user: user,
+            page,
+            size: pageSize
+        })
     } catch (error) {
         next(error)
     }
