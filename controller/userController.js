89300e890b6353ae900be0e71355682f4386c1b6
lm-hossain <aleam.hossain@mymdoc.com>
2024-04-10 15:09:15 +0600

    auth middleware  done

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,6 @@
 exports.getAllUserController = async (req,res,next)=>{
     try {
+        console.log(req.user);
         res.status(200).json({msg:"test message"})
     } catch (error) {
         next(error)
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,6 +1,7 @@
 
 
 const {getAllUserController} = require('../controller/userController');
+const { auth } = require('../middlewares/authMiddleware');
 const Router = require('../utils/method');
 function test(req, res) {
     return res.end('from test function');
@@ -10,7 +11,7 @@ function routerFun(req,res){
     const router = new  Router(req, res);
 
     router.get('/',(req,res,next)=>{console.log('test from middle');next()},(req,res,next)=>{console.log('test from middle2');next()},test);
-    router.get('/all',getAllUserController);
+    router.get('/all',auth, getAllUserController);
     router.delete('/all',(req,res,next)=>res.end('from user post all function'));
     router.end();
 
